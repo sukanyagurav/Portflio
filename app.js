ba889c2b81eb41a8fc98766b1e5f8e20d6ce71ca
sukanya gurav <sukanyagurav6@gmail.com>
2024-04-03 12:25:50 +0530

    updated design

--- app.js
+++ app.js
@@ -5,30 +5,45 @@ console.log(data)
 let curr= 0
 function createProject(newData){
     const main = document.querySelector('.project-container')
-    // let newData = data.slice(curr,curr+3)
+
     const articles= newData.map(project=>{
+      console.log(project)
             return `<article class="card" >
-                            <img src="${project.imageUrl}" alt="" class="card_image">
-                            <div class="buttons">
-                      <a href="${project.liveLink}" target="_blank">
-                        <i class="fa-solid fa-up-right-from-square" aria-hidden="true"></i>
+              
+
+                    <div class="project__image">
+                    <img loading="lazy" src="${project.imageUrl}" alt="" class="card_image">
+                  </div>
+                  <div class="description">
+                    <a href="${project.liveLink}" target="_blank" class="projectName">
+                    ${project.name}
+                    </a>
+                    <a href="${project.githubLink}" target="_blank">
+                        <i class="fa-brands fa-square-github" aria-hidden="true"></i>
                       </a>
-                      <a href="${project.githubLink}" target="_blank">
-                        <i class="fa-brands fa-github" aria-hidden="true"></i>
-                      </a>
-                    </div>
+                      <div class="card__technology">
+                        ${createList(project?.technology)}
+                      </div>
+                  </div>
                     </article>`
     
         })
         main.innerHTML+=articles.join('')
         console.log(newData)
 }
+function createList(lists){
+  console.log(lists)
+  return lists.map(item =>`<span>${item}</span>`).join(' ')
+}
 loadMore.addEventListener('click',(e)=>
 {
     e.target.classList.add('show-loader')
+    console.log(e.target)
+    e.target.setAttribute('disabled',true)
     let newData 
     setTimeout(()=>{
         e.target.classList.remove('show-loader')
+        e.target.removeAttribute('disabled')
         newData = data.slice(curr,curr+3)
         createProject(newData)
     },3000)
--- projectsData.js
+++ projectsData.js
@@ -2,53 +2,74 @@ export default [
     
     {
         id:1,
-        name:'expenses chart component main',
+        name:'Expenses chart component main',
         imageUrl:'./src/img/Projects/expenses-chart-component-main.jpg',
         liveLink:'https://sukanyagurav.github.io/expenses-chart-component-main/',
-        githubLink:'https://github.com/sukanyagurav/expenses-chart-component-main'
+        githubLink:'https://github.com/sukanyagurav/expenses-chart-component-main',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:2,
-        name:'faq accordion card main',
+        name:'FAQ accordion card main',
         imageUrl:'./src/img/Projects/faq-accordion-card-main.jpg',
         liveLink:'https://sukanyagurav.github.io/faq-accordion-card-main/',
-        githubLink:'https://github.com/sukanyagurav/faq-accordion-card-main/tree/master'
+        githubLink:'https://github.com/sukanyagurav/faq-accordion-card-main/tree/master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:3,
-        name:'fylo dark theme landing page master',
+        name:'Fylo dark theme landing page master',
         imageUrl:'./src/img/Projects/fylo-dark-theme-landing-page-master.jpg',
         liveLink:'https://sukanyagurav.github.io/fylo-dark-theme-landing-page-master/',
-        githubLink:'https://github.com/sukanyagurav/fylo-dark-theme-landing-page-master'
+        githubLink:'https://github.com/sukanyagurav/fylo-dark-theme-landing-page-master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:4,
-        name:'notifications page main',
+        name:'Notifications page main',
         imageUrl:'./src/img/Projects/notifications-page-main.jpg',
         liveLink:'https://sukanyagurav.github.io/Notifications-page/',
-        githubLink:'https://github.com/sukanyagurav/Notifications-page'
+        githubLink:'https://github.com/sukanyagurav/Notifications-page',
+        technology:[
+            'HTML','CSS','Javascript'
+        ]
     },
 
     {
         id:5,
-        name:'interactive card details form main',
+        name:'Interactive card details form main',
         imageUrl:'./src/img/Projects/interactive-card-details-form-main.jpg',
         liveLink:'https://sukanyagurav.github.io/Interactive-card-details-form-/',
-        githubLink:'https://github.com/sukanyagurav/Interactive-card-details-form-'
+        githubLink:'https://github.com/sukanyagurav/Interactive-card-details-form-',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:6,
-        name:'newsletter sign up with success message main',
+        name:'Newsletter sign up with success message main',
         imageUrl:'./src/img/Projects/newsletter-sign-up-with-success-message-main.jpg',
         liveLink:'https://sukanyagurav.github.io/newsletter-sign-up-with-success-message-main/',
-        githubLink:'https://github.com/sukanyagurav/newsletter-sign-up-with-success-message-main'
+        githubLink:'https://github.com/sukanyagurav/newsletter-sign-up-with-success-message-main',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:7,
         name:'Coding bootcamp testimonials slider',
         imageUrl:'./src/img/Projects/carousel.jpg',
         liveLink:'https://sukanyagurav.github.io/coding-bootcamp-testimonials-slider-master/',
-        githubLink:'https://github.com/sukanyagurav/coding-bootcamp-testimonials-slider-master'
+        githubLink:'https://github.com/sukanyagurav/coding-bootcamp-testimonials-slider-master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
 
     },
     {
@@ -56,72 +77,102 @@ export default [
         name:'Sunnyside agency landing page',
         imageUrl:'./src/img/Projects/sunnyside-agency-landing-page-main.jpg',
         liveLink:'https://sukanyagurav.github.io/Sunnyside-agency-landing-page/',
-        githubLink:'https://github.com/sukanyagurav/Sunnyside-agency-landing-page'
+        githubLink:'https://github.com/sukanyagurav/Sunnyside-agency-landing-page',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:9,
-        name:'intro section with dropdown navigation main',
+        name:'Intro section with dropdown navigation main',
         imageUrl:'./src/img/Projects/intro-section-with-dropdown-navigation-main.jpg',
         liveLink:'https://sukanyagurav.github.io/Intro-section-with-dropdown-navigation/',
-        githubLink:'https://github.com/sukanyagurav/Intro-section-with-dropdown-navigation'
+        githubLink:'https://github.com/sukanyagurav/Intro-section-with-dropdown-navigation',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:10,
-        name:'interactive rating component main',
+        name:'Interactive rating component main',
         imageUrl:'./src/img/Projects/interactive-rating-component-main.jpg',
         liveLink:'https://sukanyagurav.github.io/interactive-rating-component-main/',
-        githubLink:'https://github.com/sukanyagurav/interactive-rating-component-main/tree/master'
+        githubLink:'https://github.com/sukanyagurav/interactive-rating-component-main/tree/master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
    
     {
         id:11,
-        name:'pricing component with toggle master',
+        name:'Pricing component with toggle master',
         imageUrl:'./src/img/Projects/pricing-component-with-toggle-master.jpg',
         liveLink:'https://sukanyagurav.github.io/pricing-component-with-toggle-master/',
-        githubLink:'https://github.com/sukanyagurav/pricing-component-with-toggle-master'
+        githubLink:'https://github.com/sukanyagurav/pricing-component-with-toggle-master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:12,
-        name:'testimonials grid section main',
+        name:'Testimonials grid section main',
         imageUrl:'./src/img/Projects/testimonials-grid-section-main.jpg',
         liveLink:'https://sukanyagurav.github.io/testimonials-grid-section-main/',
-        githubLink:'https://github.com/sukanyagurav/testimonials-grid-section-main/tree/master'
+        githubLink:'https://github.com/sukanyagurav/testimonials-grid-section-main/tree/master',
+        technology:[
+            'HTML','CSS','Animation'
+        ]
     },
     {
         id:13,
-        name:'news homepage main',
+        name:'News homepage main',
         imageUrl:'./src/img/Projects/news-homepage-main.jpg',
         liveLink:'https://sukanyagurav.github.io/news-homepage-main/',
-        githubLink:'https://github.com/sukanyagurav/news-homepage-main/tree/master'
+        githubLink:'https://github.com/sukanyagurav/news-homepage-main/tree/master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
 
     {
         id:14,
-        name:'social proof section master',
+        name:'Social proof section master',
         imageUrl:'./src/img/Projects/social-proof-section-master.jpg',
         liveLink:'https://sukanyagurav.github.io/social-proof-section-master/',
-        githubLink:'https://github.com/sukanyagurav/social-proof-section-master/tree/master'
+        githubLink:'https://github.com/sukanyagurav/social-proof-section-master/tree/master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     {
         id:15,
-        name:'results summary component main',
+        name:'Results summary component main',
         imageUrl:'./src/img/Projects/results-summary-component-main.jpg',
         liveLink:'https://sukanyagurav.github.io/results-summary-component-main/',
-        githubLink:'https://github.com/sukanyagurav/results-summary-component-main'
+        githubLink:'https://github.com/sukanyagurav/results-summary-component-main',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
     
     {
         id:16,
-        name:'fylo data storage component master',
+        name:'Fylo data storage component master',
         imageUrl:'./src/img/Projects/Project 7.jpg',
         liveLink:'https://sukanyagurav.github.io/fylo-data-storage-component-master/',
-        githubLink:'https://github.com/sukanyagurav/fylo-data-storage-component-master/tree/master'
+        githubLink:'https://github.com/sukanyagurav/fylo-data-storage-component-master/tree/master',
+        technology:[
+            'HTML','CSS','Animation'
+        ]
     },
     {
         id:17,
-        name:'game landing page',
+        name:'Game landing page',
         imageUrl:'./src/img/Projects/game landing page.JPG',
         liveLink:'https://sukanyagurav.github.io/game-landing-page/',
-        githubLink:'https://github.com/sukanyagurav/game-landing-page/tree/master'
+        githubLink:'https://github.com/sukanyagurav/game-landing-page/tree/master',
+        technology:[
+            'HTML','CSS','Javascript','Animation'
+        ]
     },
 ]
